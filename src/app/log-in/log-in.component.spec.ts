9506007af8c7caeef46e0cd02624c8887f92588d
Bozhiyeh <ddd22251444@gmail.com>
2019-12-20 18:31:08 +0800

    add log-in spec

--- src/app/log-in/log-in.component.spec.ts
+++ src/app/log-in/log-in.component.spec.ts
@@ -1,25 +1,37 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogInComponent } from './log-in.component';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+
 
 describe('LogInComponent', () => {
+  let authService: AuthService;
+  let router: Router;
+  let http: HttpClient;
   let component: LogInComponent;
-  let fixture: ComponentFixture<LogInComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LogInComponent ]
-    })
-    .compileComponents();
-  }));
+  let validators: any[];
+  let asyncValidators: any[];
+  let loginForm = new NgForm(validators, asyncValidators);
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LogInComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    authService = new AuthService(http);
+    component = new LogInComponent(authService, router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should cancel', ()=> {
+    let spyObj = spyOn(loginForm, 'reset');
+    component.cancel(loginForm);
+    expect(spyObj).toHaveBeenCalledTimes(1);
+  })
+  it('shoulde login', () => {
+    let spyObj = spyOn(authService, 'login');
+    component.login(loginForm);
+    expect(spyObj).toHaveBeenCalledTimes(1);
   });
 });
